70babcd4fd8bb6d40af71a3b305f1436e8c97f1e
Chengcheng Liu <liu.cheng@northeastern.edu>
2022-02-09 10:29:27 -0500

    update dao

---
+++ .history/daos/UserDao_20220207233215.ts
@@ -0,0 +1,20 @@
+import User from "../models/User";
+import UserModel from "../mongoose/UserModel";
+import UserDaoI from "../interfaces/UserDao";
+export default class UserDao implements UserDaoI {
+    async findAllUsers(): Promise<User[]> {
+        return await UserModel.find();
+    }
+    async findUserById(uid: string): Promise<User> {
+        return await UserModel.findById(userId);
+    }
+    async createUser(user: User | null): Promise<void> {
+        return await UserModel.create(user);
+    }
+    async deleteUser(uid: string): Promise<any> {
+        return await UserModel.deleteOne({ _id: userId });
+    }
+    async updateUser(uid: string, user: User): Promise<any> {
+        return await UserModel.updateOne({ _id: userId }, { $set: user });
+    }
+}
---
+++ .history/mongoose/UserModel_20220207093411.ts
@@ -0,0 +1,3 @@
+import mongoose from "mongoose";
+import UserSchema from "./UserSchema";
+const UserModel = mongoose.model('UserModel', UserSchema); export default UserModel;
