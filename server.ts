589a0d1295085df3d6462e18f3d348ffa1628a76
Chengcheng Liu <liu.cheng@northeastern.edu>
2022-02-25 10:56:01 -0500

    js doc

--- server.ts
+++ server.ts
@@ -1,3 +1,15 @@
+/**
+ * @file Implements an Express Node HTTP server. Declares RESTful Web services
+ * enabling CRUD operations on the following resources:
+ * <ul>
+ *     <li>users</li>
+ *     <li>tuits</li>
+ *     <li>likes</li>
+ * </ul>
+ *
+ * Connects to a remote MongoDB instance hosted on the Atlas cloud database
+ * service
+ */
 import express, {Request, Response} from 'express';
 
 //import controller
@@ -32,5 +44,9 @@ const followController = FollowController.getInstance(app);
 const bookmarkController = BookmarkController.getInstance(app);
 const messageController = MessageController.getInstance(app);
 
+/**
+ * Start a server listening at port 4000 locally
+ * but use environment variable PORT on Heroku if available.
+ */
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
